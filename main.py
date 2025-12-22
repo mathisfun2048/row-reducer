8b35f7c41de2f373b0a68933302b14937e90a9b3
Arya Chakrabarti <aryamallickchakrabarti@gmail.com>
2025-12-22 13:05:31 -0500

    added stuff

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+# Input: space = same row
+
+def get_matrix():
+    r = int(input("number of rows: "))
+    c = int(input("number of cols: "))
+    matrix = []
+
+    print("enter row by row, space seperated: \n")
+    for _ in range(r):
+        row = list(map(float, input(f"Row {_+1}: ").split()))
+        if len(row) != c:
+            raise ValueError(f"inccorect number of values, each row needs to have {c} numbers")
+        matrix.append(row)
+        
+    return matrix
