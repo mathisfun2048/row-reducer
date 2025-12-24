3871c7bc59c82b8fd56afff1866de2621a3c5d02
Arya Chakrabarti <aryamallickchakrabarti@gmail.com>
2025-12-23 20:19:30 -0500

    vercel stuff

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.py'],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names && assetInfo.names[0] && assetInfo.names[0].endsWith('.py')) {
+            return 'assets/[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 })
